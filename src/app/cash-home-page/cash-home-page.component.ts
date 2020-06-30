39d5f7a8de2fa8bfc2cd315e13a72a538657edfe
Sai karthik <saikarthik952@gmail.com>
2020-06-30 16:26:50 +0530

    Search Function Added

--- src/app/cash-home-page/cash-home-page.component.ts
+++ src/app/cash-home-page/cash-home-page.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import {MyServiceService, Patient} from '../my-service.service';
+
+
 
 @Component({
   selector: 'app-cash-home-page',
@@ -7,9 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CashHomePageComponent implements OnInit {
 
-  constructor() { }
+  getpatientform : FormGroup;
+  p:Patient;
+  failuremessage:string;
+  constructor(private service:MyServiceService) { }
 
   ngOnInit(): void {
+    this.getpatientform= new FormGroup({
+      ws_pat_id:new FormControl('',Validators.required)
+    });
   }
+
+  getpatient() {
+
+    console.log();
+    this.service.getpatient( this.getpatientform.value)
+      .subscribe(
+        data => {
+          this.p=data;
+          this.failuremessage=null;
+          console.log(data);
+          //this.reloadData();
+        },
+        error => {this.failuremessage="Patient Not Found";
+        this.p=null;
+      }        );
+  }
+
 
 }
